b5df8c636674c7ce619637a9cd1507c781f4e73b
terence <terenceshawn18@outlook.com>
2023-11-21 10:31:45 +0300

    created a form element inside the return brackets

--- src/Search.js
+++ src/Search.js
@@ -2,7 +2,13 @@ import React from "react";
 
 function Search({setSearchTerm}){
  const handlesearch=(e)=>{
-    
- }
+
+ };
+
+  return(
+    <form>
+        
+    </form>
+  )
 }
 export default Search;

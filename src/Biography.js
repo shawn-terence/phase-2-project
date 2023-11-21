38eba5f578879ea825c1d8643727ff13b896f27e
victor <victor.njutha@student.moringaschool.com>
2023-11-21 14:34:55 +0300

    changed codes

--- src/Biography.js
+++ src/Biography.js
@@ -1,62 +1,26 @@
 import React from "react";
-import { useState,useEffect } from "react";
-import { useParams,useHistory } from "react-router-dom"
+const Biography = ({ biography,work,appearance,connections }) => (
+    <div>
+      <h2>Biography</h2>
+      <p>Full Name: {biography['full-name']}</p>
+      <p>Alter Egos: {biography['alter-egos']}</p>
 
-export default function Biography( ) {
-    const { id }=useParams()
-    
-    const [getBiography,setBiography]=useState([])
- 
-    const [getConnection,setConnection]=useState([])
-    const history=useHistory()
-    const goBack=() =>{
-        history.push('/')
-    }
-    useEffect(() =>{
-        (
-          async() =>{
-               try {
-                
-                 const response=await fetch(`https://www.superheroapi.com/api.php/122102502218121324/search/${id}`)
-                 const data=await response.json()
-                  console.log(data.biography)
-               
-                 setBiography(data.biography)
-                 setConnection(data.connections)
-                 }
-               catch(e)
-               {
-                  console.log(e)
-                  
-               }
-          })  ()
-   
+      <h3>Work</h3>
+      <ul>
+        <li>Occupation: {work.occupation}</li>
+        <li>Base: {work.base}</li>
+      </ul>
 
-},[id])
+     <h4>Appearance</h4>
+     <ul>
+        <li>Eyecolor: {appearance.eyecolor}</li>
+        <li>Haircolor: {appearance.haircolor}</li>
+    </ul>
 
-return (
-        
-    <div className="Biography">
+    <h5>Conections</h5>
+    <ul>
+        <li>Relatives: {connections.relatives}</li>
+    </ul>
 
-      <h3>FULL NAME: <br/>{getBiography.fullName}</h3>
-      <br/>
-      <h3>ALTER EGOS: <br/>{getBiography.alterEgos}</h3>
-      <br/>
-      <h3>PLACE OF BIRTH:  <br/>{getBiography.placeOfBirth}</h3>
-      <br/>
-      <h3>FIRST APPEARANCE: <br/> {getBiography.firstAppearance}</h3>
-      <br/>
-      <h3>ALIGNMENT:  <br/>{getBiography.alignment}</h3>
-      <br/>
-     
-      <button className="button_style" onClick={() =>goBack()}>Go Back</button>
-    
-     
-
-
-  </div>
- 
-  
- 
-)
-}
+</div>
+  );

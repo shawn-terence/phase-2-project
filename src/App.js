df833981bd4c64aca9e48088faa3c603adbf4eea
terence <terenceshawn18@outlook.com>
2023-11-21 09:46:48 +0300

    added content to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
 
+import React, { useState, useEffect } from 'react';
+import Search from './Search';
+import PowerStats from './PowerStats';
+import Biography from './Biography';
+import Image from './Image';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [superheroData, setSuperheroData] = useState(null);
+
+  useEffect(() => {
+    function fetchData() {
+      const apiUrl = `https://www.superheroapi.com/api.php/122102502218121324/search/${searchTerm}`;
+      
+      fetch(apiUrl)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          const filteredResults = data.results.filter(result =>
+            result.name.toLowerCase().includes(searchTerm.toLowerCase())
+          );
+          setSuperheroData(filteredResults);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+    }
+
+    if (searchTerm) {
+      fetchData();
+    }
+  }, [searchTerm]);
+
   return (
-    <div className="App">
+    <div>
+      <Search setSearchTerm={setSearchTerm} />
+
+      {superheroData && superheroData.length > 0 ? (
+        superheroData.map(result => (
+          <div key={result.id}>
+            <Image imageUrl={result.image.url} Name={result.name}/>
+            <PowerStats powerstats={result.powerstats} />
+            <Biography biography={result.biography} work={result.work} placeofBirth={result.placeofBirth}/>
+           
+          </div>
+        ))
+      ) : (
+        <p>No results found</p>
+      )}
     </div>
   );
 }

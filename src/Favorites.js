642bf4df7048f641bf2207314d2a335a45bc3f82
butch0012 <elnathanmburu@gmail.com>
2023-11-22 10:19:36 +0300

    added fuction and imports

---
+++ src/Favorites.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PowerStats from './PowerStats';
+import Biography from './Biography';
+import Image from './Image';
+import { Link } from 'react-router-dom';
+const Favorites = ({ favorites }) => {
+    return (
+        <div>
+            <h2>Favorites</h2>
+            <Link to="/">Home</Link> 
+        </div>
+    )
+}

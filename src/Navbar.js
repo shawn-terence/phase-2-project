002a30f1f182b503b2effcf8db4dcb3b155179db
Peejay Gathaiya <gathaiya.peejay@gmail.com>
2023-11-22 09:56:31 +0300

    added <nav> element in place of <div>

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-const Navbar() {
+const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+        
+    </nav>
   )
 }
 

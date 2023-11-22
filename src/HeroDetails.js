a5a0daef9f48186190ebc3e21b09b30d383fe3f6
victor <victor.njutha@student.moringaschool.com>
2023-11-22 10:04:31 +0300

    added useeffect

--- src/HeroDetails.js
+++ src/HeroDetails.js
@@ -5,5 +5,12 @@ const HeroDetails = () => {
     const { id } = useParams();
     const [heroDetails, setHeroDetails] = useState(null);
 
+    useEffect(() => {
+        const fetchHeroDetails = async () => {
+          try {
+            const response = await fetch(https://www.superheroapi.com/api.php/122102502218121324/${id});
+            if (!response.ok) {
+              throw new Error(Error fetching hero details. Status: ${response.status});
+            }
 
-}
+

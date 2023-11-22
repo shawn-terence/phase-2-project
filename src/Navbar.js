f673bc6652bf0f44e34308ee10bc027c5b0f5727
Peejay Gathaiya <gathaiya.peejay@gmail.com>
2023-11-22 09:58:10 +0300

    added <ul> as a child of <nav>

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav>
-        
+        <ul>
+
+
+
+        </ul>
     </nav>
   )
 }

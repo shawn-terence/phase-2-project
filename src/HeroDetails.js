e24d177f7ba60ba3a86dedbf271693f69c1b911d
victor <victor.njutha@student.moringaschool.com>
2023-11-22 10:03:26 +0300

    added details

--- src/HeroDetails.js
+++ src/HeroDetails.js
@@ -1,2 +1,9 @@
 import React,{useState,useEffect} from "react";
 import {useParams,Link} from 'react-router-dom';
+
+const HeroDetails = () => {
+    const { id } = useParams();
+    const [heroDetails, setHeroDetails] = useState(null);
+
+
+}

a89378ab328b5fb71a6d28879d0f83c7db2446ae
terence <terenceshawn18@outlook.com>
2023-11-23 10:15:44 +0300

    fixed image issues

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className='nav'>
+      
 
             <button className='nav-btn'><Link to = "/">Home</Link></button>
             <button className='nav-btn'><Link to = "/favorites">Favourites</Link></button>
